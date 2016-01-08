76cee49f175f5511266376bd60c498f665012b65
Marcus Gabaldo <gabaldo@gmail.com>
2016-01-08 20:52:18 -0200

    Tanks

--- eventex/subscriptions/tests/test_view_detail.py
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -37,5 +37,5 @@ class SubscriptionDetailGet(TestCase):
 
 class SubscriptionDetailNotFound(TestCase):
     def test_not_found(self):
-        resp = self.client.get('inscricao/0/')
+        resp = self.client.get('/inscricao/0/')
         self.assertEqual(404, resp.status_code)
