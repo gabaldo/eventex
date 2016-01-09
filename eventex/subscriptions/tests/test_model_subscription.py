acf4322c1baccb643ae1d166ebebe2d08cc6f370
Marcus Gabaldo <gabaldo@gmail.com>
2016-01-09 14:12:03 -0200

    adiciona campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -24,3 +24,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Marcus Gabaldo', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By defualt paid must be False."""
+        self.assertEqual(False, self.obj.paid)
